96172b77f142dd263e00c6d8885e7751293a8002
Quentin <quentopp@gmail.com>
2017-12-01 22:50:30 -0800

    Added gtkrc and Xresources

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get install ghc libx11-dev
 sudo apt-get install wget
 sudo apt-get install zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 wget http://hackage.haskell.org/package/cabal-install-2.0.0.1/cabal-install-2.0.0.1.tar.gz
 tar xzf cabal-install-2.0.0.1.tar.gz
